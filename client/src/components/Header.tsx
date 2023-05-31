b0786b1bacefdc4f311c18ba2d22d49874665e09
Am4nso <24820651+Am4nso@users.noreply.github.com>
2023-05-31 16:07:58 +0400

    Created header.

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,25 @@
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1>UAEU News</h1>
+            <nav>
+                <div>
+                    Updates
+                </div>
+                <div>
+                    Events
+                </div>
+                <div>
+                    Opinions
+                </div>
+            </nav>
+        </header>
+    );
+
+}
+
+
+export default Header;
